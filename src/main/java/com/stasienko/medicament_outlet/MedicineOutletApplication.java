c6750b8154224bc6fd1b56c9961522a0bd78ea32
Zbigniew Stasienko <zbyszekstasienko@gmail.com>
2024-10-02 18:40:28 +0200

    adding spring boot to project

---
+++ src/main/java/com/stasienko/medicament_outlet/MedicineOutletApplication.java
@@ -0,0 +1,13 @@
+package com.stasienko.medicament_outlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineOutletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicineOutletApplication.class, args);
+    }
+
+}
