75af3835a4cb6f69f7aef93ce19bc63376b13197
Zbigniew Stasienko <zbyszekstasienko@gmail.com>
2024-10-10 13:02:54 +0200

    working second migration

---
+++ src/main/resources/db/migration/V2__first_database.sql
@@ -0,0 +1,55 @@
+CREATE TABLE Pictures (
+    id UUID PRIMARY KEY,
+    picture TEXT NOT NULL
+);
+
+CREATE TABLE Pharmacy (
+    id UUID PRIMARY KEY,
+    picture_id UUID REFERENCES Pictures(id),
+    address TEXT NOT NULL,
+    phone_number TEXT,
+    name TEXT NOT NULL,
+    email TEXT,
+    opening_hours TEXT
+);
+
+CREATE TABLE Medicine (
+    id UUID PRIMARY KEY,
+    pharmacy_id UUID REFERENCES Pharmacy(id),
+    picture_id UUID REFERENCES Pictures(id),
+    name TEXT NOT NULL,
+    description TEXT,
+    size INTEGER
+);
+
+CREATE TABLE Product (
+    id UUID PRIMARY KEY,
+    medicine_id UUID REFERENCES Medicine(id),
+    expiration_date DATE,
+    amount INTEGER,
+    is_reserved BOOLEAN,
+    base_price DOUBLE PRECISION,
+    price DOUBLE PRECISION
+);
+
+CREATE TABLE "User" (
+    id UUID PRIMARY KEY,
+    name TEXT NOT NULL,
+    surname TEXT NOT NULL
+);
+
+CREATE TABLE Reservation (
+    id UUID PRIMARY KEY,
+    user_id UUID REFERENCES "User"(id),
+    pharmacy_id UUID REFERENCES Pharmacy(id),
+    realization_date DATE,
+    is_realized BOOLEAN,
+    status INTEGER
+);
+
+CREATE TABLE ReservedProducts (
+    id UUID PRIMARY KEY,
+    product_id UUID REFERENCES Product(id),
+    reservation_id UUID REFERENCES Reservation(id),
+    amount INTEGER
+);
