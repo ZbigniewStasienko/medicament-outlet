9b4ac22c58bd605402d2d2ff5c61d998de017fcb
Zbigniew Stasienko <zbyszekstasienko@gmail.com>
2024-10-26 14:21:22 +0200

    sorting works

--- src/main/java/com/stasienko/service/ProductService.java
+++ src/main/java/com/stasienko/service/ProductService.java
@@ -59,6 +59,46 @@ public class ProductService {
         return out;
     }
 
+    public List<Product> getSortedByDistance(double latitude, double longitude) {
+        List<Product> products = getAllProducts();
+
+        products.sort((p1, p2) -> {
+            double distanceToP1 = calculateDistance(
+                    latitude,
+                    longitude,
+                    Double.parseDouble(p1.getMedicine().getPharmacy().getLatitude()),
+                    Double.parseDouble(p1.getMedicine().getPharmacy().getLongitude())
+            );
+            double distanceToP2 = calculateDistance(
+                    latitude,
+                    longitude,
+                    Double.parseDouble(p2.getMedicine().getPharmacy().getLatitude()),
+                    Double.parseDouble(p2.getMedicine().getPharmacy().getLongitude())
+            );
+            return Double.compare(distanceToP1, distanceToP2);
+        });
+
+        return products;
+    }
+
+    private double calculateDistance(
+            double lat1, double lon1,
+            double lat2, double lon2
+    ) {
+        final int EARTH_RADIUS_KM = 6371;
+
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return EARTH_RADIUS_KM * c;
+    }
+
     @PreDestroy
     public void onShutdown() {
         List<Product> toBeRestored = getProductsFromCart();
